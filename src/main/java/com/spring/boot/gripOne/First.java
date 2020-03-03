7bcd3d69a457dd6e2c484856e8a4e6e42b6713dd
basantpatidar33@gmail.com <basantpatidar33@gmail.com>
2020-03-02 20:21:44 -0500

    first commit

---
+++ src/main/java/com/spring/boot/gripOne/First.java
@@ -0,0 +1,25 @@
+package com.spring.boot.gripOne;
+
+public class First {
+	private String name;
+	private String city;
+	private int cell;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public int getCell() {
+		return cell;
+	}
+	public void setCell(int cell) {
+		this.cell = cell;
+	}
+}
