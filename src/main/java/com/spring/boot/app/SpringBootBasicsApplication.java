a1f3c2c14aabbfa25d52a3de0eb9e9a1e42c84e0
basantpatidar <basantpatidar33@gmail.com>
2020-03-25 22:32:48 -0400

    config update

---
+++ src/main/java/com/spring/boot/app/SpringBootBasicsApplication.java
@@ -0,0 +1,18 @@
+package com.spring.boot.gripOne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class SpringBootBasicsApplication {
+	
+	
+
+	public static void main(String[] args) {
+		ConfigurableApplicationContext context = SpringApplication.run(SpringBootBasicsApplication.class, args);
+		First f = context.getBean(First.class);
+		f.display();
+	}
+
+}
