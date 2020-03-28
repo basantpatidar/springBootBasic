d37efb0494dc44a98ce14e719243e1bfa1d2327c
basantpatidar <basantpatidar33@gmail.com>
2020-03-27 22:27:39 -0400

    controller update

--- src/main/java/com/spring/boot/app/rest/SignupFormRest.java
+++ src/main/java/com/spring/boot/app/rest/SignupFormRest.java
@@ -2,11 +2,27 @@ package com.spring.boot.app.rest;
 
 import javax.inject.Singleton;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.spring.boot.app.model.SignupDTO;
+import com.spring.boot.app.service.SignupService;
 
 @Singleton
 @RestController
 public class SignupFormRest {
+	@Autowired 
+	private SignupService signupService;
 	@RequestMapping(value="/signup" , method= RequestMethod.POST)
+	public SignupDTO signup(@RequestBody SignupDTO signup) {
+		signupService.createSignup(signup);
+		return signup;
+	}
+
+//	"Basant","Patidar","b@p.com","basantpatidar","aabb"
+
 	
 }
