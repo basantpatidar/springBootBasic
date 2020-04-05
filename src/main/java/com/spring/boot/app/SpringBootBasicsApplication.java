ae7f327c23cf43071d83ed2d196b69b0e2b442c1
basantpatidar <basantpatidar33@gmail.com>
2020-04-04 21:51:13 -0400

    i18

--- src/main/java/com/spring/boot/app/SpringBootBasicsApplication.java
+++ src/main/java/com/spring/boot/app/SpringBootBasicsApplication.java
@@ -1,7 +1,13 @@
 package com.spring.boot.app;
 
+import java.util.Locale;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
 
 @SpringBootApplication
 public class SpringBootBasicsApplication {
@@ -12,5 +18,20 @@ public class SpringBootBasicsApplication {
 //		ConfigurableApplicationContext context = SpringApplication.run(SpringBootBasicsApplication.class, args);
 		SpringApplication.run(SpringBootBasicsApplication.class, args);
 		}
+	
+	@Bean
+	public LocaleResolver localeResolver() {
+		AcceptHeaderLocaleResolver locale = new AcceptHeaderLocaleResolver();
+		locale.setDefaultLocale(Locale.ENGLISH);
+		return locale;
+	}
+	
+	@Bean
+	public ResourceBundleMessageSource messageSource() {
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		messageSource.setBasename("messages");
+		return messageSource;
+	}
+	
 
 }
