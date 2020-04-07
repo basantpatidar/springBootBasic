b4073fdd36446da6160027385b69aa8bce044bf2
basantpatidar <basantpatidar33@gmail.com>
2020-04-06 23:26:12 -0400

    custom exception

---
+++ src/main/java/com/spring/boot/app/exception/UserExistException.java
@@ -0,0 +1,15 @@
+package com.spring.boot.app.exception;
+
+public class UserExistException extends Exception{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7462673422498220764L;
+
+	public UserExistException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+}
