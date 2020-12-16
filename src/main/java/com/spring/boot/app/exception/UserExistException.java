848ac2dae37e438b7d55e373077bece18b004ee4
basantpatidar <basantpatidar33@gmail.com>
2020-12-16 10:58:21 -0500

    removed unused imports

--- src/main/java/com/spring/boot/app/exception/UserExistException.java
+++ src/main/java/com/spring/boot/app/exception/UserExistException.java
@@ -1,9 +1,5 @@
 package com.spring.boot.app.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-
 public class UserExistException extends Exception{
 
 	/**
